cf9c5d6ee984d12b80a5977a6f3d8d46d8f41715
ShashankS-02 <97773334+ShashankS-02@users.noreply.github.com>
2022-07-01 08:57:47 +0530

    correct lcs

--- lcs.cpp
+++ lcs.cpp
@@ -2,20 +2,38 @@
 using namespace std;
 int lcs( string s1, string s2, int m, int n)
 {
-    int dp[m][n];
-    if(m==0 || n==0){
-        dp[m][n]=0;
-    }
+    int dp[m+1][n+1];
+    bool arr[m]= {false};
+    for(int i=0; i<=m; i++)
+    {
+        for(int j=0; j<=n; j++)
+        {
+            if(i==0 || j==0){
+                dp[i][j]=0;
+                if(s1[i]==s2[j])
+                arr[i]=true;
+            }
 
-    else if(s1[m]==s2[n]){
-        dp[m][n] = 1 + lcs(s1,s2,m-1,n-1);
-    }
+            else if(s1[i]==s2[j]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+                arr[i]=true;
+            }
 
-    else {
-        dp[m][n]= max( lcs(s1,s2,m-1,n) , lcs(s1,s2,m,n-1));
+            else {
+                dp[i][j]= max( dp[i-1][j] , dp[i][j-1]);
+            }
+        }
     }
-
+    
+    for(int i=0; i<m; i++)
+    {
+        if(arr[i]==true)
+        {
+            cout<<s1[i]<<" ";
+        }
+    }
     return dp[m][n];
+
 }
 int main(){
     string s1, s2;
