661c0c0073a82b9b89ffcfc2725a55931bb5c881
Shashank_Shandilya <shashankshandiliya2002@gmail.com>
2022-07-07 17:18:34 +0530

    Correct lcs

--- lcs.cpp
+++ lcs.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-void lcs( string s1, string s2, int m, int n)
+int lcs( string s1, string s2, int m, int n)
 {
     int dp[m+1][n+1];
+    fill(dp[0],dp[0]+(m+1)*(n+1),0);
     for(int i=0; i<=m; i++)
     {
         for(int j=0; j<=n; j++)
@@ -11,7 +12,7 @@ void lcs( string s1, string s2, int m, int n)
                 dp[i][j]=0;
             }
 
-            else if(s1[i]==s2[j]){
+            else if(s1[i-1]==s2[j-1]){
                 dp[i][j] = 1 + dp[i-1][j-1];
             }
 
@@ -21,30 +22,24 @@ void lcs( string s1, string s2, int m, int n)
         }
     }
 
-     cout<<"The longest common subsequence is of length: "<<dp[m][n];
+    //  cout<<"The longest common subsequence is of length: "<<dp[m][n]<<endl;
 
-    // string ele="";
-    // int cnt=0,i=m,j=n;
-    // while(cnt!=dp[m][n] && i>=0 && j>=0)
-    // {
-    //     if(s1[i]==s2[j]){
-    //     ele+=s1[i];
-    //     i--;
-    //     j--;
-    //     cnt++;
-    //     }
-    //     else if(dp[i-1][j]>=dp[i][j-1]){
-    //         i--;
-    //     }
-    //     //else j--;
-    // }
-      
-    //  cout<<"\nThe selected elements are: "<<ele;
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<dp[m][n];
+    return dp[m][n];
 }
 int main(){
     string s1, s2;
     cout<<"Enter the strings s1 and s2:\n";
     cin>>s1>>s2;
-    lcs(s1, s2, s1.length(), s2.length());
+    int res=lcs(s1, s2, s1.length(), s2.length());
+    cout<<"The longest common subsequence is of length: "<<res;
     return 0;
 }
